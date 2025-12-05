811ca756bf00bf80aea29d0c8711503a0bb0b7da
bellten <belten.lee@lgcns.com>
2025-12-05 13:25:49 +0900

    Firebase 연동 및 Imgbb 연동 추가

---
+++ src/services/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDhzYJszFFqRrjeeGEfYnRgMGQ9vCBFLVA",
+    authDomain: "my-wedding-db.firebaseapp.com",
+    projectId: "my-wedding-db",
+    storageBucket: "my-wedding-db.firebasestorage.app",
+    messagingSenderId: "774512887690",
+    appId: "1:774512887690:web:932b27c24da04de02b6a73",
+    measurementId: "G-W8JVCC7X25"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
